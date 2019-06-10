b39e8faae808d9e89f3d1b7473334d2710c0d94d
ericjulander <jul15001@byui.edu>
2019-06-10 10:01:34 -0600

    Started playing around with JSON with LINQ

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using CsvHelper;
 using System.Collections.Generic;
 using CityData;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace charp
 {
     class Program
@@ -14,23 +17,76 @@ namespace charp
             // guy.introduceYourself();
 
             var cityData = extractCityData(readCsvFromPath("./test.csv"));
-            sortCityByRegion(cityData);
+            var regions = sortCityByRegion(cityData);
+            foreach (var region in regions)
+            {
+                var states = sortCitiesByState(region);
+                for (var i = 0; i < states.Count; i++)
+                {
+                    states[i] = sortCityCityByPopulation(states[i]);
+                    foreach (var city in states[i])
+                    {
+                        JArray jsonCity = new JArray();
+
+                        city.printCityData();
+                    }
+                }
+
+            }
             // foreach (var city in cityData)
             //     city.printCityData();
 
         }
 
-        public static void sortCityByRegion(List<City> cityData)
+        public static List<List<City>> sortCityByRegion(List<City> cityData)
         {
+            var sortedRegions = new List<List<City>>();
             var regionQuery = from city in cityData
                               group city by city.Region into regions
                               select regions;
             foreach (var region in regionQuery)
             {
-                Console.WriteLine(region.Key);
+                var tmpList = new List<City>();
                 foreach (var city in region)
+                {
+                    tmpList.Add(city);
                     city.printCityData();
+                }
+
+                sortedRegions.Add(tmpList);
             }
+
+            return sortedRegions;
+        }
+
+        public static List<List<City>> sortCitiesByState(List<City> region)
+        {
+            var stateList = new List<List<City>>();
+            var stateQuery = from city in region group city by city.State into states select states;
+            var firstTime = true;
+            foreach (var state in stateQuery)
+            {
+                var tmpList = new List<City>();
+                ;
+                foreach (var city in state)
+                {
+                    if (firstTime)
+                    {
+
+                        firstTime = false;
+                    }
+                    tmpList.Add(city);
+                    stateList.Add(tmpList);
+                }
+            }
+            return stateList;
+        }
+
+        public static List<City> sortCityCityByPopulation(List<City> cityData)
+        {
+            var sortedCityQuery = from cities in cityData.OrderBy(city => city.Population) select cities;
+            return sortedCityQuery.ToList();
+
         }
 
         public static List<City> extractCityData(List<string[]> cityData)
